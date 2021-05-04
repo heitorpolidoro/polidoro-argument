ba68c18ff544652151182de09b0632d6f48e1bbd
heitor <heitor.polidoro@gmail.com>
2021-05-04 14:21:22 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -3,24 +3,23 @@ Setup to create the package
 """
 import setuptools
 
-import polidoro_argument_old
+import polidoro_argument
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='polidoro-argument',
-    version=polidoro_argument_old.VERSION,
+    version=polidoro_argument.VERSION,
     description='Package to create command line arguments for Python.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/heitorpolidoro/polidoro-argument',
     author='Heitor Polidoro',
-    # author_email='heitor.polidoro',
     license='unlicense',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
